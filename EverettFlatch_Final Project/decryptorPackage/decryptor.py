c3b3f404d7c119530d92e71d7022fe63b508a249
Jacob Brumfield <brumfijb@mail.uc.edu>
2025-04-29 16:28:08 -0400

    Added necessary decryption files as well as the image our team took

--- EverettFlatch_Final Project/decryptorPackage/decryptor.py
+++ EverettFlatch_Final Project/decryptorPackage/decryptor.py
@@ -1,4 +1,26 @@
-class decryptor(object):
-    """description of class"""
+# File Name : decryptor.py
+# Student Name: Jacob Brumfield
+# email:  brumfijb@mail.uc.edu
+# Assignment Number: Final Project
+# Due Date:   5/1/2025
+# Course #/Section:   IS 4010 001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:  This assignment decrypts a hidden location and movie.
+# Brief Description of what this module does. Decrypts the location of the building that we must take a picture at.
+# Citations: 
+# Anything else that's relevant:
+def decrypt_location(encrypted_data, word_list):
+    """
+    This function decrypts the hidden location on campus
+    Returns the location
+    """
+    decrypted_words = []
+    for index_str in encrypted_data:
+        index = int(index_str)
+        if 0 <= index < len(word_list):
+            decrypted_words.append(word_list[index])
+        else:
+            raise IndexError(f"Index {index} out of range.")
+    return ' '.join(decrypted_words)
 
 
--- EverettFlatch_Final Project/decryptorPackage/fernet_decryptor.py
+++ EverettFlatch_Final Project/decryptorPackage/fernet_decryptor.py
@@ -1,4 +1,24 @@
-class fernet_decryptor(object):
-    """description of class"""
-
+# File Name : fernet_decryptor.py
+# Student Name: Jacob Brumfield
+# email:  brumfijb@mail.uc.edu
+# Assignment Number: Final Project!
+# Due Date:   5/1/2025
+# Course #/Section:   IS 4010 001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:  This assignment decrypts a hidden location and movie.
+# Brief Description of what this module does. Decrypts the location of the building that we must take a picture at.
+# Citations: 
+# Anything else that's relevant:
+from cryptography.fernet import Fernet
+def decrypt_movie(encrypted_message, key):
+    """
+    This function decrypts the movie
+    Returns: The movie our team was assigned
+    """
+    try:
+        fernet = Fernet(key)
+        decrypted = fernet.decrypt(encrypted_message.encode())
+        return decrypted.decode()
+    except Exception as e:
+        return f"[Decryption failed: {e}]"
 
